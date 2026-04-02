6fe1547069f004fe28d7ed48798e773da5372783
shamrin-ui1 <shamrin.uii@gmail.com>
2026-04-02 20:53:35 +0530

    Update WelcomeCard.jsx

--- Frontend/src/WelcomeCard.jsx
+++ Frontend/src/WelcomeCard.jsx
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router-dom";
 
-function WelcomeCard() {
-  const navigate = useNavigate();   
+function WelcomeCard({user}) {
+  const navigate = useNavigate();    
 
   return (
     <div className="welcome-card">
       <h2>Welcome back 👋</h2>
       <p>Your civic engagement is making a difference.</p>
 
+        {user && user.role ==="citizen" && (
       <button onClick={() => navigate("/create-petition")}>
         Create Petition
       </button>
+       )}
     </div>
   );
 }
